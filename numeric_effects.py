22b587e05ca19ac74324cd9e6ada5b1155940c28
Gary Doran, Jr <garydoranjr@gmail.com>
2019-11-30 20:34:01 -0800

    add extra printout to verify numeric effect behavior
    Ensure that parametrized increment/decrement behavior is properly
    implemented
    
    Issue: #2

--- numeric_effects.py
+++ numeric_effects.py
@@ -13,10 +13,12 @@ def problem(verbose):
             ),
             preconditions=(
                 ('>', ('quantity', 'p'), 0),
+                ('>=', ('customer-money',), ('price', 'p')),
             ),
             effects=(
                 ('-=', ('quantity', 'p'), 1),
                 ('+=', ('account',), ('price', 'p')),
+                ('-=', ('customer-money',), ('price', 'p')),
             ),
         ),
     ))
@@ -27,6 +29,7 @@ def problem(verbose):
         },
         init=(
             ('=', ('account',), 0),
+            ('=', ('customer-money',), 15),
             ('=', ('quantity', 'apples'), 10),
             ('=', ('quantity', 'oranges'), 10),
             ('=', ('price', 'apples'), 3),
@@ -37,12 +40,15 @@ def problem(verbose):
         )
     )
 
-    plan = planner(problem, verbose=verbose)
+    plan = planner(problem, verbose=True)
     if plan is None:
         print('No Plan!')
     else:
+        state = problem.initial_state
         for action in plan:
             print(action)
+            state = state.apply(action)
+            print(state.f_dict)
 
 if __name__ == '__main__':
     from optparse import OptionParser
